fe4005965109769edb55f27658aac6654d9560f2
Wiktor Mociun <voter101@gmail.com>
2014-07-12 21:30:29 +0200

    Red test for no input

---
+++ tdd_kata1/StringCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_kata1 {
+    public class StringCalculator {
+        public int Solve(string input) {
+            return 1; 
+        }
+    }
+}
---
+++ tdd_kata1_tests/StringCalculatorTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tdd_kata1;
+
+namespace tdd_kata1_tests {
+    [TestClass]
+    public class StringCalculatorTest {
+
+        public StringCalculator stringCalculator = new StringCalculator();
+
+        [TestMethod]
+        public void solveNoInput() {
+            Assert.AreEqual( stringCalculator.Solve( "" ), 0 );
+        }
+    }
+}
